dc1e324e9acc5b94c34ec4560a699e8f8a993186
Mariano R Nigro <109207084+marianusmac@users.noreply.github.com>
2022-08-09 23:09:20 -0300

    agrregado dto skill

---
+++ src/main/java/com/portfolio/mrn/Dto/dtoSkill.java
@@ -0,0 +1,29 @@
+package com.portfolio.mrn.Dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import javax.validation.constraints.NotBlank;
+@Getter @Setter
+public class dtoSkill {
+
+	@NotBlank
+	private String nombreSkill;
+	@NotBlank
+	private String porcentajeSkill;
+	@NotBlank
+	private String imagenSkill;
+
+	//Constructores
+
+	public dtoSkill() {
+	}
+
+	public dtoSkill(String nombreSkill, String porcentajeSkill, String imagenSkill) {
+		this.nombreSkill = nombreSkill;
+		this.porcentajeSkill = porcentajeSkill;
+		this.imagenSkill = imagenSkill;
+	}
+
+	//Getters and Setters x Lombok
+
+}
